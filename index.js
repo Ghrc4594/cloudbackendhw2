fe8ea94c8f6112abd965df5d38494d67c1e2a47d
Ghrc4594 <gottipatihanumantarao@gmail.com>
2025-04-15 18:05:46 -0500

    Added index.js file

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 // MySQL connection config
 // triggering azure deployment
 const db = mysql.createConnection({
-  host: 'mysqlclouddevavanti.mysql.database.azure.com',
-  user: 'mysqlAvanti',
-  password: 'Avanthi@123',
+  host: 'sampledbhanu.mysql.database.azure.com',
+  user: 'hanu',
+  password: 'Hanu@123',
   database: 'studentdb',
   port: 3306,
   ssl: {
